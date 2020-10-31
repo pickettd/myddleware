3501b1f7b6116634b0ae9bd8af3f549874bca5df
David Pickett <pickettd@gmail.com>
2020-10-30 17:48:39 -1000

    Add child1 and child2 emails to list of Moodle field metadata

--- src/Myddleware/RegleBundle/Custom/Solutions/lib/moodle/metadata.php
+++ src/Myddleware/RegleBundle/Custom/Solutions/lib/moodle/metadata.php
@@ -3,3 +3,5 @@
   Testing
 */
 $moduleFields['users']['customfields'] = array('label' => 'Custom Fields', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0);
+$moduleFields['users']['child1_email'] = array('label' => 'Child1 Email', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0);
+$moduleFields['users']['child2_email'] = array('label' => 'Child2 Email', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0);
